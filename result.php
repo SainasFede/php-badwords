167ffeb845bd69f6e72d25af8a9d2e19bd12610d
SainasFede <sainasfede@gmail.com>
2022-11-30 15:40:28 +0100

    add exercist

---
+++ result.php
@@ -0,0 +1,27 @@
+<?php
+
+  $word = $_POST['word'];
+  $paragrafo = $_POST['paragrafo'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stampo</title>
+</head>
+<body>
+  
+  <h1>La Parola è <?php echo $word ?></h1>
+
+  <p><?php echo $paragrafo ?></p>
+  <h4>Il Paragrafo contiene <?php echo strlen($paragrafo) ?> tra caratteri e spazi</h4>
+
+  <h1>Censuriamo alcune parole</h1>
+  <p><?php echo str_replace('ciao','***',$paragrafo) ?></p>
+
+</body>
+</html>
